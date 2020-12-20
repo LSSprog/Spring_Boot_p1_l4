09bd6821beb26e441dd780fc66ea36495cd559a1
LSS <fedexss@bk.ru>
2020-12-21 00:05:56 +0300

    first commit

---
+++ src/test/java/sring_boot/project/Hw4ApplicationTests.java
@@ -0,0 +1,13 @@
+package sring_boot.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
